ff585bfc26ccacf1b12dc16f3c207603c4231a5a
jhermsmeier <jhermsmeier@googlemail.com>
2015-04-02 21:08:35 +0200

    Updated test/update: Expanded triple for assertion

--- test/update.js
+++ test/update.js
@@ -1,19 +1,19 @@
 var HexaDB = require( '..' )
 var assert = require( 'assert' )
 
-var db = new HexaDB( require( 'memdb' )() )
-
-var triples = [{
-  s: 'jonas',
-  p: 'friend',
-  o: 'computer',
-},{
-  s: 'computer',
-  p: 'friend',
-  o: 'jonas'
-}]
-
 suite( 'Update', function() {
+  
+  var db = new HexaDB( require( 'memdb' )() )
+  
+  var triples = [{
+    subject: 'jonas',
+    predicate: 'friend',
+    object: 'computer',
+  },{
+    subject: 'computer',
+    predicate: 'friend',
+    object: 'jonas',
+  }]
   
   setup( function() {
     triples.forEach( function( triple ) {
@@ -24,16 +24,16 @@ suite( 'Update', function() {
   test( 'db#update', function( next ) {
     
     var replacement = {
-      s: 'jonas',
-      p: 'owner',
-      o: 'computer'
+      subject: 'jonas',
+      predicate: 'owner',
+      object: 'computer',
     }
     
     db.update( triples[0], replacement, function( error ) {
       if( error ) return next( error )
       db.get({ s: 'jonas', p: 'owner' }, function( error, result ) {
         if( error ) return next( error )
-        assert.equal( result[0].predicate, 'owner' )
+        assert.deepEqual( result[0], replacement )
         next()
       })
     })
