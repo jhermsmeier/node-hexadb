77f6ed3132711a79772554a05bd0a2739f12e06e
jhermsmeier <jhermsmeier@googlemail.com>
2015-04-02 21:08:02 +0200

    Updated test/search: UI -> TDD, moved init -> setup()

--- test/search.js
+++ test/search.js
@@ -1,53 +1,53 @@
 var HexaDB = require( '..' )
 var assert = require( 'assert' )
 
-var db = new HexaDB( require( 'memdb' )() )
-
-// Thanks to 'levelgraph' for this test case,
-// I took the liberty of stealing it. Shame on me.
-var triples = [{
-  subject: 'matteo',
-  predicate: 'friend',
-  object: 'daniele'
-},{
-  subject: 'daniele',
-  predicate: 'friend',
-  object: 'matteo'
-},{
-  subject: 'daniele',
-  predicate: 'friend',
-  object: 'marco'
-},{
-  subject: 'lucio',
-  predicate: 'friend',
-  object: 'matteo'
-},{
-  subject: 'lucio',
-  predicate: 'friend',
-  object: 'marco'
-},{
-  subject: 'marco',
-  predicate: 'friend',
-  object: 'davide'
-}]
-
-var query = [{
-  subject: 'matteo',
-  predicate: 'friend',
-  object: HexaDB.Variable( 'x' )
-},{
-  subject: HexaDB.Variable( 'x' ),
-  predicate: 'friend',
-  object: HexaDB.Variable( 'y' )
-},{
-  subject: HexaDB.Variable( 'y' ),
-  predicate: 'friend',
-  object: 'davide'
-}]
-
-describe( 'Search', function() {
+suite( 'Search', function() {
   
-  it( 'prepare', function() {
+  var db = new HexaDB( require( 'memdb' )() )
+  
+  // Thanks to 'levelgraph' for this test case,
+  // I took the liberty of stealing it. Shame on me.
+  var triples = [{
+    subject: 'matteo',
+    predicate: 'friend',
+    object: 'daniele'
+  },{
+    subject: 'daniele',
+    predicate: 'friend',
+    object: 'matteo'
+  },{
+    subject: 'daniele',
+    predicate: 'friend',
+    object: 'marco'
+  },{
+    subject: 'lucio',
+    predicate: 'friend',
+    object: 'matteo'
+  },{
+    subject: 'lucio',
+    predicate: 'friend',
+    object: 'marco'
+  },{
+    subject: 'marco',
+    predicate: 'friend',
+    object: 'davide'
+  }]
+  
+  var query = [{
+    subject: 'matteo',
+    predicate: 'friend',
+    object: HexaDB.Variable( 'x' )
+  },{
+    subject: HexaDB.Variable( 'x' ),
+    predicate: 'friend',
+    object: HexaDB.Variable( 'y' )
+  },{
+    subject: HexaDB.Variable( 'y' ),
+    predicate: 'friend',
+    object: 'davide'
+  }]
+  
+  setup( function() {
     // Since we're using MemDB for testing here,
     // we omit checking for errors, or waiting for the callback
     // (we can omit checking for errors, because the primitive ops
@@ -57,7 +57,7 @@ describe( 'Search', function() {
     })
   })
   
-  it( 'db#searchStream()', function( done ) {
+  test( 'db#searchStream()', function( done ) {
     
     var result = []
     
@@ -70,7 +70,6 @@ describe( 'Search', function() {
         }
       })
       .on( 'end', function() {
-        // console.log( 'RESULT', result )
         assert.equal( result.length, 1 )
         assert.deepEqual( result.shift(), { x: 'daniele', y: 'marco' } )
         done()
@@ -78,7 +77,7 @@ describe( 'Search', function() {
     
   })
   
-  it( 'db#search()', function( done ) {
+  test( 'db#search()', function( done ) {
     
     db.search( query, function( error, result ) {
       assert.equal( result.length, 1 )
