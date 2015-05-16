379dadef918f8a62e501a7611ae3a265545d7c23
Jonas Hermsmeier <jhermsmeier@gmail.com>
2015-05-16 16:16:19 +0200

    Updated tests

--- test/triple.js
+++ test/triple.js
@@ -1,19 +1,20 @@
 var HexaDB = require( '..' )
 var assert = require( 'assert' )
+var Triple = HexaDB.Triple
 
 describe( 'Triple', function() {
   
   it( 'detect sparse triples', function() {
-    var t = new HexaDB.Triple({ p: 'friend' })
-    assert.ok( t.isSparse )
-    var t = new HexaDB.Triple({ s: 'herbert', p: 'friend' })
-    assert.ok( t.isSparse )
-    var t = new HexaDB.Triple({ s: 'herbert' })
-    assert.ok( t.isSparse )
-    var t = new HexaDB.Triple({ s: 'herbert', o: 'hildegard' })
-    assert.ok( t.isSparse )
-    var t = new HexaDB.Triple({ o: 'hildegard' })
-    assert.ok( t.isSparse )
+    var t = new Triple({ p: 'friend' })
+    assert.ok( Triple.isSparse( t ) )
+    var t = new Triple({ s: 'herbert', p: 'friend' })
+    assert.ok( Triple.isSparse( t ) )
+    var t = new Triple({ s: 'herbert' })
+    assert.ok( Triple.isSparse( t ) )
+    var t = new Triple({ s: 'herbert', o: 'hildegard' })
+    assert.ok( Triple.isSparse( t ) )
+    var t = new Triple({ o: 'hildegard' })
+    assert.ok( Triple.isSparse( t ) )
   })
   
 })
